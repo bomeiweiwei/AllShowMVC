776319ad88fa53764f568bee00c34a6483705760
吉米巴特 <bomeilonch@msn.com>
2021-02-22 21:38:33 +0800

    npm

--- AllShowMVC/App_Start/BundleConfig.cs
+++ AllShowMVC/App_Start/BundleConfig.cs
@@ -9,10 +9,11 @@ namespace AllShowMVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/node_modules/jquery/dist/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/node_modules/jquery-validation/dist/jquery.validate.js",
+                        "~/Scripts/node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js"));
 
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
@@ -20,10 +21,18 @@ namespace AllShowMVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/node_modules/bootstrap/dist/js/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/vue").Include(
+                      "~/Scripts/node_modules/vue/dist/vue.js",
+                      "~/Scripts/node_modules/bootstrap-vue/dist/bootstrap-vue.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/axios").Include(
+                      "~/Scripts/node_modules/axios/dist/axios.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Scripts/node_modules/bootstrap/dist/css/bootstrap.css",
+                      "~/Scripts/node_modules/bootstrap-vue/dist/bootstrap-vue.min.css",
                       "~/Content/site.css"));
         }
     }
