dd5c9487521479e23b4c82972f7ef58d97cb0671
wei <bomeilonch0210@gmail.com>
2021-02-24 11:41:59 +0800

    Identity

---
+++ AllShowMVC.Models/IdentityModel/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllShowMVC.Models.IdentityModel
+{
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //這必需在第一行
+            base.OnModelCreating(modelBuilder);
+            //以下依實際情境來調整 table name
+            modelBuilder.Entity<CustomUser>().ToTable("AllShowUsers");
+            modelBuilder.Entity<IdentityRole>().ToTable("AllShowRoles");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("AllShowUserRoles");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("AllShowUserClaims");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("AllShowUserLogins");
+        }
+    }
+}
---
+++ AllShowMVC/App_Start/Startup.Auth.cs
@@ -0,0 +1,37 @@
+﻿using AllShowMVC.Models.IdentityModel;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddIdentity
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Register"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, CustomUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+        }
+    }
+}
